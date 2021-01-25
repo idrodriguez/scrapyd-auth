d5406c76ea1e37170efeade45f9c5349a929ffab
idrodriguez <irodriguez.salguero@gmail.com>
2021-01-25 15:28:52 +0100

    Fixed issue when creating directories

--- run_container.sh
+++ run_container.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
 htpasswd -b -c /etc/nginx/htpasswd $USERNAME $PASSWORD
 nginx
-cd /code
-mkdir logs
-mkdir eggs
-logparser -dir /code/logs -t 10 --delete_json_files --scrapyd_server 127.0.0.1:6800 & scrapyd
+
+SCRAPY_LOGS="/code/logs"
+SCRAPY_EGGS="/code/eggs"
+
+if [ ! -d $SCRAPY_LOGS ] 
+then
+        mkdir $SCRAPY_LOGS
+        echo "Creating logs folder"
+fi
+
+if [ ! -d $SCRAPY_EGGS ] 
+then
+        mkdir $SCRAPY_EGGS
+        echo "Creating eggs folder"
+fi
+
+logparser -dir $SCRAPY_LOGS -t 10 --delete_json_files --scrapyd_server 127.0.0.1:6800 & scrapyd
